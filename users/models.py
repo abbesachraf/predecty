e72d043e5cd757d74cc45bca192ebc3964be80de
colonneila <abbesachraf@gabes.r-iset.tn>
2022-06-14 13:33:23 +0100

    make it better

--- users/models.py
+++ users/models.py
@@ -32,8 +32,3 @@ class Result1 (models.Model):
     def __str__(self):
         return self.rzlt
     
-class Order(models.Model):
-    product_category = models.CharField(max_length=20)
-    payment_method = models.CharField(max_length=50)
-    shipping_cost = models.CharField(max_length=50)
-    unit_price = models.DecimalField(max_digits=5, decimal_places=2)
